d7505487a2cd906034deae931cf4e25fde0f7683
Mandy Chung <mandy.chung@oracle.com>
2021-03-09 13:51:43 -0800

    add thread confinement check

--- src/java.base/share/classes/jdk/internal/util/ArrayBuilder.java
+++ src/java.base/share/classes/jdk/internal/util/ArrayBuilder.java
@@ -10,7 +10,8 @@ import java.util.Objects;
  */
 public class ArrayBuilder<E> {
     private static final Unsafe UNSAFE = Unsafe.getUnsafe();
-    private final E[] array;      // ## make thread confinement?
+    private final E[] array;
+    private Thread owner;
 
     @SuppressWarnings("unchecked")
     public ArrayBuilder(Class<?> arrayClass, int length) {
@@ -22,6 +23,10 @@ public class ArrayBuilder<E> {
         return this;
     }
 
+    public E get(int index) {
+        return array[index];
+    }
+
     public ArrayBuilder<E> copy(E[] original) {
         System.arraycopy(original, 0, array, 0, original.length);
         return this;
@@ -29,6 +34,17 @@ public class ArrayBuilder<E> {
 
     @SuppressWarnings("unchecked")
     public E[] build() {
-        return (E[]) Objects.requireNonNull(UNSAFE.freezeLarvalArray(array));
+        checkState();
+        E[] a = (E[])Objects.requireNonNull(UNSAFE.freezeLarvalArray(array));
+        close();
+        return a;
+    }
+
+    private void checkState() {
+        if (owner != Thread.currentThread())
+            throw new IllegalStateException(owner == null ? "closed builder" : "wrong thread");
+    }
+    private void close() {
+        owner = null;
     }
 }
